444c9e61b4e89115a977095c931b63ba1ee4b0d6
Victoria Reynolds <toireyn@gmail.com>
2017-11-23 16:20:34 +0000

    Fix: added to the equation

--- calc.py
+++ calc.py
@@ -5,6 +5,9 @@ pb=22
 turtule=25
 numberBought=3
 specialBought = 2
+cherryBought=2
+pbBought=2
+turtuleBought=1
 total=numberBought* cheeseCake+ specialBought* special
 print 'You ordered 5 cheesecakes and that is going to be $90.'
-print 'You ordered ' + str(numberBought+specialBought)+ ' cheesecakes and that is going to be $'+str( total)+'.'
+print 'You ordered ' + str(numberBought+specialBought+cherryBought+pbBought+turtuleBought)+ ' cheesecakes and that is going to be $'+str( total)+'.'
