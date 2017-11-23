ba6c64c71541c47f0705cb7efd6fc763eb09a909
Victoria Reynolds <toireyn@gmail.com>
2017-11-23 01:30:25 +0000

    feature: added calculations to calc.py

---
+++ calc.py
@@ -0,0 +1,7 @@
+cheeseCake =18
+special = 22
+numberBought=3
+specialBought = 2
+total=numberBought* cheeseCake+ specialBought* special
+print 'You ordered 5 cheesecakes and that is going to be $90.'
+print 'You ordered ' + str(numberBought+specialBought)+ ' cheesecakes and that is going to be $'+str( total)+'.'
